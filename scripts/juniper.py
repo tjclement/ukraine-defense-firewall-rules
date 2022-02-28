94883f8744e1ff7f3c3329fa2bdab6bbf847dc66
Tom <mr@tomclement.nl>
2022-02-28 15:10:08 +0100

    Fixed /32 not being applied to last entry in Juniper list

--- scripts/juniper.py
+++ scripts/juniper.py
@@ -2,4 +2,4 @@ def juniper_save(lists):
     for name, sublists in lists.items():
         for subname, ips in sublists.items():
             with open(f"juniper/greynoise_{name}_{subname}.txt", "wt") as file:
-                file.write("/32\n".join(ips))
+                file.write("\n".join([ip + "/32" for ip in ips]))
